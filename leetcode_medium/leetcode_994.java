8aced4d58cdf41fa5ab7aab06b5f3a51e948f0cb
brian.jia <brian.jia@snowflake.com>
2020-04-11 22:03:02 -0400

    easy_level

---
+++ leetcode_medium/leetcode_994.java
@@ -0,0 +1,19 @@
+package leetcode_medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class leetcode_994 {
+    public static void main(String[] args) {
+
+    }
+
+    public int orangesRotting(int[][] grid) {
+        return 0;
+    }
+
+
+    public ArrayList getAdjacent(int x, int y, int[][] grid){
+        return null;
+    }
+}
