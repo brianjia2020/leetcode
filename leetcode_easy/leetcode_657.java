32382a46106e4c01c9ae93dcf073616c1e7ffafd
brian.jia <brian.jia@snowflake.com>
2020-04-16 08:41:52 -0400

    some more

---
+++ leetcode_easy/leetcode_657.java
@@ -0,0 +1,23 @@
+package leetcode_easy;
+
+import org.junit.Test;
+
+public class leetcode_657 {
+    @Test
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for (char input: moves.toCharArray()){
+            if (input == 'U'){
+                y++;
+            } else if (input == 'D'){
+                y--;
+            } else if (input == 'L'){
+                x--;
+            } else if (input == 'R'){
+                x++;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+}
