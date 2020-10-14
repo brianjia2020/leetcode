05a751e3a53a0380c88161361ee8e9c680f9eb39
bjia <brianjia1123@gmail.com>
2020-10-14 09:09:27 -0400

    344

---
+++ leetcode_easy/leetcode_344.java
@@ -0,0 +1,24 @@
+package leetcode_easy;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class leetcode_344 {
+    @Test
+    public void test(){
+        char[] s = "12".toCharArray();
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+
+    public void reverseString(char[] s) {
+        char temp;
+        for (int i = 0; i < (s.length/2); i++ ){
+            temp = s[i];
+            s[i] = s[s.length-1-i];
+            s[s.length-1-i] = temp;
+        }
+    }
+
+}
