f110341ff5c8f611f955ed291f6536cbcce6b00e
bjia <brianjia1123@gmail.com>
2020-11-11 09:50:15 -0500

    78

--- leetcode_easy/leetcode_448.java
+++ leetcode_easy/leetcode_448.java
@@ -1,11 +1,13 @@
 package leetcode_easy;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class leetcode_448 {
 
-    public List<Integer> findDisappearedNumbers(int[] nums) {
+    public List<Integer> findDisappearedNumbers(int @NotNull [] nums) {
         List<Integer> ll = new LinkedList<>();
         int [] r = new int[nums.length];
         for(int x: nums) {
@@ -14,7 +16,6 @@ public class leetcode_448 {
         for(int i=0;i<r.length;i++){
             if (r[i] == 0) ll.add(i+1);
         }
-
         return ll;
     }
 }
