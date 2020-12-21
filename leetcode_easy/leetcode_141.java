6a975e9d5646906e259fbfb677441608bee28207
bjia <brianjia1123@gmail.com>
2020-12-21 12:36:08 -0500

    142

--- leetcode_easy/leetcode_141.java
+++ leetcode_easy/leetcode_141.java
@@ -1,5 +1,7 @@
 package leetcode_easy;
 
+import java.util.HashSet;
+
 public class leetcode_141 {
     public boolean hasCycle(leetcode_206.ListNode head) {
         if (head == null) return false;
@@ -13,6 +15,20 @@ public class leetcode_141 {
                 slow = slow.next;
                 fast = fast.next.next;
             }
+        }
+        return false;
+    }
+
+    public boolean hasCycle2(leetcode_206.ListNode head){
+        if (head==null) return false;
+        HashSet<leetcode_206.ListNode> set = new HashSet<>();
+        while(head!=null){
+            if (set.contains(head)){
+                return true;
+            } else {
+                set.add(head);
+            }
+            head = head.next;
         }
         return false;
     }
---
+++ leetcode_medium/leetcode_973.java
@@ -0,0 +1,17 @@
+package leetcode_medium;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class leetcode_973 {
+    public int[][] kClosest(int[][] points, int K) {
+        int[][] res = new int[K][2];
+        Arrays.sort(points,(a,b) -> (int) Math.sqrt(a[0]*a[0]+a[1]*a[1]-b[0]*b[0]-b[1]*b[1]));
+        System.out.println(Arrays.deepToString(points));
+        for(int i=points.length-1;i>points.length-K-1;i--){
+            res[i][0] = points[i][0];
+            res[i][1] = points[i][1];
+        }
+        return res;
+    }
+}
