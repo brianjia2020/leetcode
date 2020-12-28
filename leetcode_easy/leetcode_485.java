7246cf01171fc463bfead0420725cece4f70217d
chunyangjia <brianjia1123@gmail.com>
2020-12-27 19:37:47 -0500

    61

---
+++ leetcode_easy/leetcode_485.java
@@ -0,0 +1,17 @@
+package leetcode_easy;
+
+public class leetcode_485 {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int temp = 0;
+        for (int i =0;i<nums.length;i++){
+            if (nums[i]==1){
+                temp++;
+                if (temp > maxCount) maxCount = temp;
+            } else {
+                temp = 0;
+            }
+        }
+        return maxCount;
+    }
+}
