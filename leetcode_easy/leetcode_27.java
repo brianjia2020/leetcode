745b296df65fd0d0b3e20f39e29304d0a9627bae
chunyangjia <brianjia1123@gmail.com>
2020-12-28 19:53:35 -0500

    26,27

---
+++ leetcode_easy/leetcode_27.java
@@ -0,0 +1,14 @@
+package leetcode_easy;
+
+public class leetcode_27 {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for(int num:nums){
+            if(num!=val){
+                nums[i] =num;
+                i++;
+            }
+        }
+        return i;
+    }
+}
