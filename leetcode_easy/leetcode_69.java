17c52f0685087858730003f94f863b8cc9217276
bjia <brianjia1123@gmail.com>
2021-01-18 14:32:15 -0500

    24

---
+++ leetcode_easy/leetcode_69.java
@@ -0,0 +1,19 @@
+package leetcode_easy;
+
+public class leetcode_69 {
+    public int mySqrt(int x){
+        if (x<2) return x;
+        int left = 2;
+        int right = x/2;
+        int mid = 0;
+        long num;
+        while(left<=right){
+            mid = left + (right-left)/2;
+            num = (long) mid*mid;
+            if(num==x) return mid;
+            if(num>x) right = mid-1;
+            else left = mid + 1;
+        }
+        return right;
+    }
+}
