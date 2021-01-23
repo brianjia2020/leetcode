f7daeb1ed07cfa2770077649d11d117aebfe6c5d
chunyangjia <brianjia1123@gmail.com>
2021-01-23 18:44:19 -0500

    commit

--- company_questions/amazon/Q2_FetchItemToDisplay.java
+++ company_questions/amazon/Q2_FetchItemToDisplay.java
@@ -12,7 +12,7 @@ public class Q2_FetchItemToDisplay {
     }
     public static List<String> fetchItemsToDisplay(int numOfItems, Map<String, PairInt> items, int sortParameter, int sortOrder, int itemsPerPage, int pageNumber){
         if(itemsPerPage*(pageNumber-1)>numOfItems||numOfItems==0) return null;
-        List<String> res = sortItems(items,sortParameter,sortOrder);
+        List<String> res = sortItems2(items,sortParameter,sortOrder);
         System.out.println(res);
         List<String> res2 = new ArrayList<>();
         for(int i=pageNumber*itemsPerPage;i<itemsPerPage*(pageNumber+1);i++){
@@ -22,6 +22,31 @@ public class Q2_FetchItemToDisplay {
         return res2;
     }
 
+    public static List<String> sortItems2(Map<String, PairInt> items, int sortParameter, int sortOrder){
+        String[] arr = items.keySet().toArray(new String[0]);
+        Arrays.sort(arr,(o1,o2)->{
+            PairInt pairInt1 = items.get(o1);
+            PairInt pairInt2 = items.get(o2);
+            if(sortParameter==0) {
+                if(sortOrder==0) {
+                    return o1.compareTo(o2);
+                } else {
+                    return o2.compareTo(o1);
+                }
+            } else if(sortParameter==1){
+                if(sortOrder==0) return Integer.compare(pairInt1.relevance,pairInt2.relevance);
+                else return Integer.compare(pairInt2.relevance,pairInt1.relevance);
+            } else if(sortParameter==2){
+                if(sortOrder==0) return Integer.compare(pairInt1.price,pairInt2.price);
+                else return Integer.compare(pairInt2.price,pairInt1.price);
+            } else {
+                return 0;
+            }
+        });
+        return Arrays.asList(arr);
+    }
+
+    //bubble sort is very slow
     public static List<String> sortItems(Map<String, PairInt> items, int sortParameter, int sortOrder){
         String[] arr = items.keySet().toArray(new String[0]);
         int z = 0;
---
+++ leetcode_hard/leetcode_1192.java
@@ -0,0 +1,64 @@
+package leetcode_hard;
+
+import java.util.*;
+
+public class leetcode_1192 {
+
+    public static void main(String[] args) {
+        int n =4;
+        List<List<Integer>> connections = new ArrayList<>();
+        connections.add(new ArrayList<>());
+        connections.get(0).add(0);
+        connections.get(0).add(1);
+        connections.add(new ArrayList<>());
+        connections.get(1).add(1);
+        connections.get(1).add(2);
+        connections.add(new ArrayList<>());
+        connections.get(2).add(2);
+        connections.get(2).add(0);
+        connections.add(new ArrayList<>());
+        connections.get(3).add(1);
+        connections.get(3).add(3);
+        System.out.println(criticalConnections(n,connections));
+    }
+
+    static List<List<Integer>> result;
+    static int time;
+    public static List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        int[] low = new int[n];
+        int[] discovery = new int[n];
+        result = new ArrayList<>();
+        time = 0;
+        List<Integer> map[] = new ArrayList[n];
+        for(int i=0;i<n;i++){
+            map[i] = new ArrayList<>();
+        }
+        for(List<Integer> connection: connections){
+            map[connection.get(0)].add(connection.get(1));
+            map[connection.get(1)].add(connection.get(0));
+        }
+        tarjan_bridge(0,map,low,discovery,-1);
+        return result;
+    }
+
+    public static void tarjan_bridge(int src,List<Integer> map[],int[] low, int[] discovery,int par){
+        discovery[src] = low[src] = time++;
+        for(int child: map[src]){
+            if(child==par) continue;
+            if(discovery[child]==0){
+                tarjan_bridge(child,map,low,discovery,src);
+                if(low[child]>discovery[src]){
+                    result.add(List.of(child,src));
+                } else {
+                    low[src] = Math.min(low[src],low[child]);
+                }
+            } else {
+                low[src] = Math.min(low[src],low[child]);
+            }
+        }
+    }
+
+
+
+
+}
